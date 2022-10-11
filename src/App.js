c94f87b0476b14ddf29656090c49d0d0a5d3cdb0
Md Mazharul Islam <mazharulislam.dev@gmail.com>
2022-10-11 13:37:08 +0600

    login and sign up page added also added background

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './componets/login/Login';
+import LoginPage from './componets/login/LoginPage';
+import SignUpPage from './componets/login/SignUpPage';
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Login />} />
+        <Route path='/' element={<Login />}>
+          <Route index element={<LoginPage />} />
+          <Route path='/signup' element={<SignUpPage />} />
+        </Route>
       </Routes>
     </div>
   );
--- src/componets/login/Login.js
+++ src/componets/login/Login.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Link, Outlet } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div>
-            <h2 className='text-3xl'>This is login page</h2>
+        <div className='h-screen'>
+            <div className='w-1/4 h-4/6 ml-60 mt-16 rounded-xl bg-gradient-to-bl from-[#515869] to-[#2c2e38]'>
+                <div className='flex justify-around items-center pt-16'>
+                    <Link to="/" className='text-gray-200 hover:text-gray-400'>Login</Link>
+                    <Link to="signup" className='text-gray-200 hover:text-gray-400'>Sign Up</Link>
+                </div>
+                <div>
+                    <Outlet />
+                </div>
+            </div>
         </div>
     );
 };
